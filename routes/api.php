616e3414142a509d0a0ece4883d9ffba2a8a8244
Avazbek Nematov <68123420+avacoderuz@users.noreply.github.com>
2022-03-29 18:34:06 +0500

    test api

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/get', function (Request $request){
+Route::patch('/get', function (Request $request){
     return $request->all();
 });
 
